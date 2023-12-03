d124866221935890b93735b6f622db7737ded9df
Guido Mantegna <gmantegna@elcallao.com>
2023-12-03 19:33:50 -0300

    Create JWT for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-import Users from "../models/users.js";
+import Users from "../models/users.js"
+import jwt from "jsonwebtoken"
 
 export class AuthController {
   static async signup(req, res, next) {
@@ -7,14 +8,24 @@ export class AuthController {
       email: req.body.email,
       password: req.body.password,
       confirmPassword: req.body.confirmPassword,
-    });
+    })
+
+    const token = jwt.sign(
+      // payload: an object for all the data that we're going to store inside of the token
+      { id: newUser._id },
+      // secret: basically a string for a secret
+      process.env.JWT_SECRET,
+      //options
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    )
 
     res.status(201).json({
       status: "success",
+      token,
       data: {
         data: newUser,
       },
-    });
-    next();
+    })
+    next()
   }
 }
