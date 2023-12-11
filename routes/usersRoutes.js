1552228e345165a7c5df44710680701849f206bb
Guido Mantegna <gmantegna@elcallao.com>
2023-12-11 18:09:39 -0300

    Get all the requests that belong to a user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { UsersController } from "../controllers/usersController.js"
 import { AuthController } from "../controllers/authController.js"
+import { requestsRouter } from "./requestsRoutes.js"
 
 export const usersRouter = Router()
 
 usersRouter.post("/signup", AuthController.signup)
 usersRouter.post("/login", AuthController.login)
 
+// usersRouter.use("/:id/requests", requestsRouter)
 usersRouter.route("/").get(UsersController.getAll)
 
 usersRouter
